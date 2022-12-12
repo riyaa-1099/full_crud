02c91322655b74582a72ef9dfced6e5c298616b1
riyaa-1099 <112823546+riyaa-1099@users.noreply.github.com>
2022-12-12 09:43:31 +0530

    hopefullt final

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express=require("express")
-
+var cors = require('cors')
 const {connection}=require("./config/db");
 const { authentication } = require("./middleware/auth");
 const {notesRouter}=require("./routes/app.route")
 const {userRouter}=require("./routes/user.route")
 
 const app=express();
-
+app.use(cors())
 app.use(express.json())
 
 app.use("/user",userRouter)
 
-app.use(authentication)
-app.use("/notes",notesRouter)
-
 app.get("/", (req, res) => {
     res.send("Welcome")
 })
+app.get("/name", (req, res) => {
+    res.send(process.env.name)
+})
+app.use(authentication)
+app.use("/notes",notesRouter)
 
 app.listen(7500,async()=>{
     try{
